ff8afd2672f737c41f6b357b0c67d1b0dbb61598
yuukilion <hy130310@gmail.com>
2025-03-29 17:07:06 +0900

    [update]ASGを利用できるように修正

--- main.tf
+++ main.tf
@@ -2,21 +2,40 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0fb653ca2d3203ac1"
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
-                echo "Hello, World" >> index.html
-                nohup busybox httpd -f -p "${var.server_port}" & 
-            EOF
+                echo "Hello, world" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+}
 
-    user_data_replace_on_change = true
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnets.default.ids
 
-    tags = {
-        Name = "terraform-example-yusei"
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                = "Name"
+        value               = "terraform-example-asg"
+        propagate_at_launch = true
     }
 }
 
